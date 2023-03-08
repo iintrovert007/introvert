a809d35dfefdef2bf80ab4a79f1951a48ee06fe3
iintrovert007 <110751039+iintrovert007@users.noreply.github.com>
2023-03-08 12:33:57 +0530

    final ecom
    final ecom

--- frontend/src/components/admin/ProductList.js
+++ frontend/src/components/admin/ProductList.js
@@ -50,7 +50,7 @@ export default function ProductList() {
             data.rows.push({
                 id: product._id,
                 name: product.name,
-                price : `$${product.price}`,
+                price : `Rs${product.price}`,
                 stock: product.stock,
                 actions: (
                     <Fragment>
